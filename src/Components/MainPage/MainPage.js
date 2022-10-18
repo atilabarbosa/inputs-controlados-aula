c3832b43906b815afc4bd9ab6b0f568125dfd795
atilabarbosa <atilacabo@gmail.com>
2022-10-18 11:28:59 -0300

    exercicio manha

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,24 +1,73 @@
-import React from 'react'
-import {MainContainer, Form, Input} from './styles'
+import React, { useState } from 'react'
+import {MainContainer, Form, Input} from './styles'  
 
 function MainPage() {
+
+  const [nome, setNome] = useState("")
+  const [idade, setIdade] = useState("")
+  const [email, setEmail] = useState("")
+
+  function onChangeNome(event){
+    //console.log(event.target.value)
+    setNome(event.target.value)
+  }
+  function onChangeIdade(event){
+    //console.log(event.target.value)
+    setIdade(event.target.value)
+  }
+  function onChangeEmail(event){
+    //console.log(event.target.value)
+    setEmail(event.target.value)
+  }
+
+  const consoleObjeto = () => {
+    const dadosUsuario = {
+      nome: nome,
+      idade: idade,
+      email: email
+    }
+    console.log(dadosUsuario)
+    setNome('')
+    setIdade('')
+    setEmail('')
+  }
+  //console.log(`nome:${nome}, idade:${idade}, e-mail:${email}`)
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
       <Form>
         <label>
           Nome:
-          <Input />
+          <Input 
+          type = {'text'}
+          placeholder = {'Nome'}
+          value={nome}
+          onChange = {onChangeNome}
+          
+          />
         </label>
         <label>
           Idade:
-          <Input/>
+          <Input
+          type = {'number'}
+          placeholder = {'Idade'}
+          value={idade}
+          onChange = {onChangeIdade}
+          
+          />
         </label>
         <label>
           E-mail:
-          <Input />
+          <Input 
+          type = {'email'}
+          placeholder = {'usuario@usuario.com'}
+          value={email}
+          onChange = {onChangeEmail}
+          />
         </label>
-      <button>Enviar dados</button>
+      <button
+      onClick = {consoleObjeto}
+      >Enviar dados</button>
       </Form>
     </MainContainer>
   )
